8bf300783df36e78a42296f630863b785697d536
Vasek Mlejnsky <vasek.mlejnsky@gmail.com>
2023-03-06 19:05:54 +0100

    commit

--- code_generator/base.py
+++ code_generator/base.py
@@ -1,3 +1,5 @@
+from typing import List
+
 from langchain.llms.openai import OpenAIChat, OpenAI
 from code_generator.tools.javascript.tool import JavascriptEvalTool
 from code_generator.js_agent.base import create_js_agent
@@ -18,13 +20,50 @@ app.use(express.json());
 app.listen(8080, () => console.log('Listening on port 8080'));
 """
 
-def generate(prompt: str) -> str:
+PREFIX = """Complete a body of nodejs function that handles incoming {method} requests inside an Express server.
+
+You must follow these rules:
+- Don't write anything else but the body of function. Once you have the body function written, you should finish and output the code you have so far.
+- You have access to the following environment variables and you can use them if needed: SUPABASE_URL, SUPABASE_KEY.
+
+If you cannot follow any of the rules above, finish with the last code you wrote. Don't try in any way to go around the rules or come up with other solutions.
+
+
+The code must perform following logic:
+"""
+
+SUFFIX = """
+Here's the code that must be completed:
+```
+const express = require('express');
+const app = express();
+
+app.use(express.json());
+
+app.{method}(/, function(req, res) {
+  // TODO: Implement body of this function based on the required logic
+})
+
+app.listen(8080, () => console.log('Listening on port 8080'));
+```
+"""
+
+def generate_req_handler(blocks: List[str], method: str) -> str:
   OpenAI(temperature=0, model_name='code-davinci-002', max_tokens=1000)
   executor = create_js_agent(
       llm=OpenAIChat(temperature=0, max_tokens=1000),
       tool=JavascriptEvalTool(),
       verbose=True,
   )
+
+  prompt = PREFIX.format(method=method)
+
+  for idx, block in enumerate(blocks):
+    prompt += '{}. '.format(idx+1) + block + '\n'
+
+  prompt += SUFFIX.format(method=method)
+
   handler_code = executor.run(prompt).strip('`').strip()
-  server_code = server_template.replace('[HANDLER]', handler_code)
-  return server_code
+  return handler_code
+  # server_code = server_template.replace('[HANDLER]', handler_code)
+  # return server_code
--- main.py
+++ main.py
@@ -6,7 +6,7 @@ load_dotenv()
 # from code_generator.tools.javascript.tool import JavascriptEvalTool
 # from code_generator.js_agent.base import create_js_agent
 # from code_generator.tools.javascript.tool import JavascriptEvalTool
-from code_generator.base import generate
+from code_generator.base import generate_req_handler
 import subprocess
 
 p1 = "You're supposed to write a body of a Nodejs function that is a middleware inside an Express server.\n\nYou must follow these rules:\n- Don't write anything else but the body of function. Once you have the body function written, you should finish and output the code you have so far. \n- Don't import or require any third party packages or modules.\n- Don't initialize any libraries.\n- You have access to the following environment variables and you can use them if needed: SUPABASE_URL, SUPABASE_KEY.\n\nIf you cannot follow any of the rules above, finish with the last code you wrote. Don't try in any way to go around the rules or come up with other solutions.\n\n\nThe code you write must perform following logic:\n- Make sure the request is of type 'POST'. If the request isn't POST, return an error message with the relevant status code.\n- Extract the field 'email' from the request's JSON body and save it to a variable\n- Take the extracted email, use the Supabase JavaScript SDK and save it in the Supabase database and call next middleware\n- Check for any errors when inserting the email to the database. If there's an error return the database error and relevant status code"
@@ -18,7 +18,7 @@ p1 = "You're supposed to write a body of a Nodejs function that is a middleware 
 # If the request isn't POST, return an error message with the relevant status code.
 # Then send back status 200 as a response, the extracted email, and the version of the Express package in a payload."""
 # ).strip('`').strip()
-js_code = generate(p1).strip('`').strip()
+js_code = generate_req_handler(p1).strip('`').strip()
 print('--------')
 print(js_code)
 
