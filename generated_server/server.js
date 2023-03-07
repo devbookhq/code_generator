9cf4d30d6288e84c5ce52f936482e5ed10692d2b
Vasek Mlejnsky <vasek.mlejnsky@gmail.com>
2023-03-07 10:14:41 +0100

    commit

--- generated_server/server.js
+++ generated_server/server.js
@@ -1,19 +1,27 @@
-function middleware(req, res, next) {
-  if(req.method !== 'POST') {
-    return res.status(400).send('Invalid request method');
+const express = require('express');
+const app = express();
+
+app.use(express.json());
+
+app.get('/', function(req, res) {
+  if (req.method !== 'GET') {
+    return res.status(405).send('Method Not Allowed');
   }
-  const email = req.body.email;
+  const { email } = req.body;
+  if (!email) {
+    return res.status(400).send('Email is required');
+  }
   const { createClient } = require('@supabase/supabase-js');
-  const supabaseUrl = process.env.SUPABASE_URL;
-  const supabaseKey = process.env.SUPABASE_KEY;
-  const supabase = createClient(supabaseUrl, supabaseKey);
+  const supabase = createClient(process.env.SUPABASE_URL, process.env.SUPABASE_KEY);
   supabase
     .from('emails')
     .insert([{ email }])
     .then(() => {
-      next();
+      return res.status(200).send('Email inserted successfully');
     })
     .catch((error) => {
       return res.status(500).send(error.message);
     });
-}
+});
+
+app.listen(8080, () => console.log('Listening on port 8080'));
